8fbc6014152aa7b908ad5d9be632bbf0fcc81932
sjagadee <srini.jagadeesh@gmail.com>
2021-04-29 20:09:09 +0530

    adding changes

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
@@ -6,42 +6,53 @@ import NewExpense from "./components/NewExpense/NewExpense";
 // custom components are uppercase, reacts detects custom components through uppercase
 // it also detects built in components through lowercase
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", 
+    title: "New TV", 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12) 
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  }
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", 
-      title: "New TV", 
-      amount: 799.49, 
-      date: new Date(2021, 2, 12) 
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expenseDataReceived) => {
-    console.log(expenseDataReceived);
-    expenses.push(expenseDataReceived);
+    setExpenses((prevExpenses) => {
+      console.log(prevExpenses);
+      console.log(expenseDataReceived);
+
+      console.log("final", [expenseDataReceived, ...prevExpenses]);
+      return [expenseDataReceived, ...prevExpenses];
+    });
+
+    // console.log(expenseDataReceived);
+    // expenses.push(expenseDataReceived);
   }
 
   return (
     <div>
       <NewExpense onAddExpenseData={addExpenseHandler}/>
-      <Expenses expenses={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 };
